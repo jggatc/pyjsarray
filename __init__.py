10fbafdfe6c693221294038c3256edc4974d72ba
James Garnon <jg.gatc@gmail.com>
2026-08-24 11:41:50 -0400

    update documentation

--- __init__.py
+++
@@ -1,5 +0,0 @@
-#PyjsArray - Copyright (C) 2013 James Garnon <https://gatc.ca/>
-#Released under the MIT License <https://opensource.org/licenses/MIT>
-
-__version__ = 0.56
-
---
+++ pyjsarray/__init__.py
@@ -0,0 +1,8 @@
+#PyjsArray - Python-to-JavaScript TypedArray Module
+#Copyright (c) 2013 James Garnon
+#Licensed under the MIT License
+#See LICENSE.txt for full license text
+
+
+__version__ = 0.56
+
